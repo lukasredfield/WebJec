4c42fa15483e1efaedf675de16ac1a079cd3cbe9
lukasredfield <lukasredfield@gmail.com>
2023-05-20 01:52:45 -0300

    save28

--- WebJecApp/models.py
+++ WebJecApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    phone_number = forms.IntegerField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- WebJecApp/views.py
+++ WebJecApp/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.core.mail import EmailMessage
 from django.contrib import messages
-from django import forms
 
 
 # Create your views here.
@@ -33,17 +32,6 @@ def index(request):
     return render(request, 'WebJecApp/Index.html')
 
 
-class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    phone_number = forms.IntegerField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
-
-
-from django.core.mail import EmailMessage
-from django.shortcuts import render
-from django.contrib import messages
-
 
 def contact_view(request):
     if request.method == 'POST':
@@ -56,26 +44,32 @@ def contact_view(request):
         # Validar que todos los campos obligatorios se han completado
         if not (name and email and phone_number and message):
             messages.error(request, 'Debe completar todos los campos para enviar el mensaje.')
-            return render(request, 'WebJecApp/Mensaje.html')
+            return render(request, 'WebJecApp/Mensaje.html', {'enviado': False})
 
         # Validar que el campo Phone_Number solo contiene números
         if not phone_number.isdigit():
             messages.error(request, 'El campo "Número de teléfono" solo debe contener números.')
-            return render(request, 'WebJecApp/Mensaje.html')
+            return render(request, 'WebJecApp/Mensaje.html', {'enviado': False})
 
         # Construir el correo electrónico
         subject = f"Nuevo mensaje de {name}"
         body = f"Nombre: {name}\nEmail: {email}\nTeléfono: {phone_number}\nMensaje: {message}"
-        from_email = 'lukasredfield01@gmail.com'
+        from_email = 'lukasredfield26@gmail.com'
         to_email = ['lukasredfield@gmail.com']
 
-        # Enviar el correo electrónico
-        email = EmailMessage(subject, body, from_email, to_email)
-        email.send()
-        messages.success(request, '¡Mensaje enviado, nos contactaremos a la brevedad! "Pidan, y se les dará; busquen, y encontrarán; llamen, y se les abrirá. 8 Porque todo el que pide, recibe; el que busca, encuentra; y al que llama, se le abrirá.  (Mateo 7:7-8)"')
-        return render(request, 'WebJecApp/Mensaje.html')
+        try:
+            # Enviar el correo electrónico
+            email = EmailMessage(subject, body, from_email, to_email)
+            email.send()
+            messages.success(request, '¡Mensaje enviado! Nos contactaremos a la brevedad. "Pidan, y se les dará; busquen, y encontrarán; llamen, y se les abrirá. Porque todo el que pide, recibe; el que busca, encuentra; y al que llama, se le abrirá. (Mateo 7:7-8)"')
+            return render(request, 'WebJecApp/Mensaje.html', {'enviado': True})
+        except Exception as e:
+            messages.error(request, 'No se ha podido enviar el mensaje. Te invitamos a que por el momento nos envíes un mensaje por medio de nuestras redes sociales. "Pidan, y se les dará; busquen, y encontrarán; llamen, y se les abrirá. Porque todo el que pide, recibe; el que busca, encuentra; y al que llama, se le abrirá. (Mateo 7:7-8)"')
+            return render(request, 'WebJecApp/Mensaje.html', {'enviado': False})
+
     else:
-        return render(request, 'WebJecApp/Mensaje.html')
+        return render(request, 'WebJecApp/Mensaje.html', {'enviado': False})
+
 
 
         
